cfd442ca6bb3f02de261f286bcd32b40c2badc2f
Ricardo <rayrtsx@proton.me>
2024-08-18 14:58:53 -0600

    hey

---
+++ src/constants/index.js
@@ -0,0 +1,27 @@
+
+
+
+export const seriesDeGuitarra = [
+    // from cort guitars
+    {
+        id: 1,
+        name: "G"
+    },
+    {
+        id: 2,
+        name: "KX"
+    },
+    {
+        id: 3,
+        name: "X"
+    },
+    {
+        id: 4,
+        name: "CR"
+    },
+    {
+        id: 5,
+        name: "Sunset"
+    },
+
+]
